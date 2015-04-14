5bb93b9dfc05edf38e36cfefc1e7ad2964c81b68
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-14 20:30:55 +0200

    BorderText: The custom control

---
+++ TheoryOfComputation/BorderText.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TheoryOfComputation
+{
+	/// <summary>
+	/// Interaction logic for BorderText.xaml
+	/// </summary>
+	public partial class BorderText : UserControl
+	{
+		public string Text
+		{
+			get { return Text; }
+			set { txt.Text = value; }
+		}
+
+		public bool Last
+		{
+			get { return Last; }
+			set { border.BorderThickness = new Thickness(0,2,0,2); }
+		}
+
+		public BorderText()
+		{
+			InitializeComponent();
+		}
+	}
+}
