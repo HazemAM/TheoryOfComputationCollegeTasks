a5af75b5424e18e1b2350e4363b10297d489da0a
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-14 16:08:24 +0200

    State: The custom control
    Also, state positions in MainWindow.

---
+++ TheoryOfComputation/State.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace TheoryOfComputation
+{
+	/// <summary>
+	/// Interaction logic for State.xaml
+	/// </summary>
+	public partial class State : UserControl
+	{
+		public int Index {
+			get { return Index; }
+			set { label.Text = value.ToString(); }
+		}
+
+		public bool IsFinal {
+			get { return IsFinal; }
+			set {
+				if(value) finalIndicator.Visibility = Visibility.Visible;
+			}
+		}
+
+		public State()
+		{
+			InitializeComponent();
+		}
+	}
+}
