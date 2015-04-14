689883c62e8bd70b8235d9c8c315e63bcb3a9fe5
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-14 18:02:57 +0200

    ArcArrow: The custom control.
    With sample code in MainWindow.

---
+++ TheoryOfComputation/ArcArrow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TheoryOfComputation
+{
+	/// <summary>
+	/// Interaction logic for Arrow.xaml
+	/// </summary>
+	public partial class ArcArrow : UserControl
+	{
+		public ArcArrow()
+		{
+			InitializeComponent();
+		}
+	}
+}
