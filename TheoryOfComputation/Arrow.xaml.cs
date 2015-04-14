008a41599131c24b0d43f146b2402a44a0c05e85
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-14 17:32:49 +0200

    Arrow: The custom control
    With MainWindow sample code and a fix in State.

---
+++ TheoryOfComputation/Arrow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TheoryOfComputation
+{
+	/// <summary>
+	/// Interaction logic for Arrow.xaml
+	/// </summary>
+	public partial class Arrow : UserControl
+	{
+		public int Length
+		{
+			get { return Length; }
+			set {
+				line.X2 = value;
+				head.Margin = new Thickness(value-5, 0, -(value-5), 0);
+			}
+		}
+
+		public Arrow()
+		{
+			InitializeComponent();
+		}
+	}
+}
