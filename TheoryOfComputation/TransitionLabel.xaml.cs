0e440db3e4afbe9d4e5b586eb7bfaac1072bd7ed
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-15 19:11:21 +0200

    TransitionLabel: The custom control

---
+++ TheoryOfComputation/TransitionLabel.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TheoryOfComputation
+{
+	/// <summary>
+	/// Interaction logic for TransitionLabel.xaml
+	/// </summary>
+	public partial class TransitionLabel : UserControl
+	{
+		public string Text
+		{
+			get { return Text; }
+			set { label.Text = value; }
+		}
+
+		public TransitionLabel()
+		{
+			InitializeComponent();
+		}
+
+		public void highlight()
+		{
+			label.Foreground = new SolidColorBrush(Color.FromRgb(255,0,0));
+		}
+
+		public void dehighlight()
+		{
+			label.Foreground = new SolidColorBrush(Color.FromRgb(0,0,0));
+		}
+	}
+}
