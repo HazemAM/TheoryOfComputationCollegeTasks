d2cfb8c8823dd64483208267e9ef2357c68ea316
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-15 18:48:21 +0200

    The 'Fast Run' UI button
    Implemented with DispatcherTimer.

--- TheoryOfComputation/MainWindow.xaml.cs
+++ TheoryOfComputation/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TheoryOfComputation
 {
@@ -12,6 +12,7 @@ namespace TheoryOfComputation
 		const char EMPTY = '\0';
 		
 		AddThreeMachine machine;
+		DispatcherTimer fastRunTimer;
 		int currentState;
 
 		public MainWindow()
@@ -30,6 +31,10 @@ namespace TheoryOfComputation
 
 			buttonStep.IsEnabled = true;
 			buttonFinish.IsEnabled = true;
+			buttonFastRun.IsEnabled = true;
+
+			if(fastRunTimer != null)
+				fastRunTimer.IsEnabled = false;
 		}
 
 		private void buttonStep_Click(object sender, RoutedEventArgs e)
@@ -46,22 +51,40 @@ namespace TheoryOfComputation
 		{
 			Tuple<int,char,Direction> output;
 
-			//TODO: Something similar to 'buttonStep_Click' (based on FINAL_STATE)?
 			while((output = machine.step()) != null)
 				handleStep(output);
 
 			disableButtons();
 		}
 
+		private void buttonFastRun_Click(object sender, RoutedEventArgs e)
+		{
+			Tuple<int,char,Direction> output;
+
+			disableButtons(); //Disable the buttons while the animation is running.
+
+			fastRunTimer = new DispatcherTimer{ Interval = TimeSpan.FromSeconds(0.25) };
+			fastRunTimer.Tick += (o,ev) =>
+			{
+				if((output = machine.step()) != null)
+					handleStep(output);
+				if(output.Item1 == AddThreeMachine.FINAL_STATE)
+					fastRunTimer.IsEnabled = false;
+			};
+			fastRunTimer.IsEnabled = true;
+		}
+
 		private void disableButtons()
 		{
 			buttonStep.IsEnabled = false;
 			buttonFinish.IsEnabled = false;
+			buttonFastRun.IsEnabled = false;
 		}
 
 		private void handleStep(Tuple<int, char, Direction> output)
 		{
-			tape.write(output.Item2);
+			if(output.Item2 != AddThreeMachine.EMPTY)
+				tape.write(output.Item2);
 			canvas.highlightState(output.Item1);
 			canvas.highlightArrow(currentState, output.Item1);
 			if(output.Item3 == Direction.RIGHT)
